2256fbbf67341a020d6a402cf25fffbae54e914a
Trịnh Hoài Đức <anhduc08768@gmail.com>
2025-12-20 17:07:10 +0700

    refactor: xóa LoadingScreen khỏi RootLayout

--- src/layout/RootLayout/RootLayout.tsx
+++ src/layout/RootLayout/RootLayout.tsx
@@ -1,7 +1,6 @@
 import classNames from 'classnames/bind';
-import { Outlet, useNavigation } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 
-import LoadingScreen from '@/components/LoadingScreen/LoadingScreen';
 import { ScrollProvider } from '@/contexts/ScrollContext';
 import Footer from '@/layout/Footer/Footer';
 import Header from '@/layout/Header/Header';
@@ -11,13 +10,11 @@ import styles from './RootLayout.module.scss';
 const cx = classNames.bind(styles);
 
 const RootLayout = () => {
-  const navigation = useNavigation();
-
   return (
     <ScrollProvider>
       <Header />
       <main className={cx('main')}>
-        {navigation.state === 'loading' ? <LoadingScreen /> : <Outlet />}
+        <Outlet />
       </main>
       <Footer />
     </ScrollProvider>
